86b8934c9518102af8cfdd151d159ea2813839c9
thebigbangflash <99984994+thebigbangflash@users.noreply.github.com>
2022-02-28 20:56:26 -0500

    Update TallyVotes.sh

--- Governance/TallyVotes.sh
+++ Governance/TallyVotes.sh
@@ -1,14 +1,26 @@
+ASAUNIT=100000000
+
+# curl the Tinyman pool to help calculate the CURRENT swap ratio of ASA to LP-TOKEN
+LPTOKENINPOOL=$(curl -X GET "https://mainnet-algorand.api.purestake.io/idx2/v2/accounts/OKN6LN3PSSB7H7HXZJ4SQ6YVOYNE6NNXG6TV4TADMFQXYIFRZQXJI5ZATE/" -H "x-api-key:API-KEY-HERE" -H 'accept: application/json' | jq -r '.account.assets[] | select (."asset-id"=='"552946222"') | .amount')
+
+ASAINPOOL=$(curl -X GET "https://mainnet-algorand.api.purestake.io/idx2/v2/accounts/OKN6LN3PSSB7H7HXZJ4SQ6YVOYNE6NNXG6TV4TADMFQXYIFRZQXJI5ZATE/" -H "x-api-key:API-KEY-HERE" -H 'accept: application/json' | jq -r '.account.assets[] | select (."asset-id"=='"393495312"') | .amount')
+
+LPOUTPOOL=$(echo 18446744073709551615-$LPTOKENINPOOL|bc)
+ASAPOOLRATIO=$(echo "scale=6; $LPOUTPOOL*$ASAUNIT/$ASAINPOOL/1000000"|bc)
+
 #########################################Gov1:A########################################
 cp /home/k/gov/Gov1-A.txt /home/k/gov/Gov1-A.bak
 
 while IFS= read -r line; do
 resultafd=$(curl -X GET "https://mainnet-algorand.api.purestake.io/idx2/v2/accounts/${line}/" -H "x-api-key:API-KEY-HERE" -H 'accept: application/json' | jq -r '.account.assets[] | select (."asset-id"==393495312) | .amount')
 
-echo $line $resultafd >> /home/k/gov/results/Gov1-A-AFD.txt
+echo $line $resultafd >> /home/k/gov/results/Gov1-A-tally.txt
 
 resultpool=$(curl -X GET "https://mainnet-algorand.api.purestake.io/idx2/v2/accounts/${line}/" -H "x-api-key:API-KEY-HERE" -H 'accept: application/json' | jq -r '.account.assets[] | select (."asset-id"==552946222) | .amount')
 
-echo $line $resultpool >> /home/k/gov/results/Gov1-A-POOL.txt
+INLIQUIDITY=$(echo $resultpool/$ASAPOOLRATIO*$ASAUNIT/1000000|bc)
+
+echo $line $INLIQUIDITY >> /home/k/gov/results/Gov1-A-tally.txt
 
 sed -i '1d' "/home/k/gov/Gov1-A.bak"
 
@@ -22,11 +34,13 @@ cp /home/k/gov/Gov1-B.txt /home/k/gov/Gov1-B.bak
 while IFS= read -r line; do
 resultafd=$(curl -X GET "https://mainnet-algorand.api.purestake.io/idx2/v2/accounts/${line}/" -H "x-api-key:API-KEY-HERE" -H 'accept: application/json' | jq -r '.account.assets[] | select (."asset-id"==393495312) | .amount')
 
-echo $line $resultafd >> /home/k/gov/results/Gov1-B-AFD.txt
+echo $line $resultafd >> /home/k/gov/results/Gov1-B-tally.txt
 
 resultpool=$(curl -X GET "https://mainnet-algorand.api.purestake.io/idx2/v2/accounts/${line}/" -H "x-api-key:API-KEY-HERE" -H 'accept: application/json' | jq -r '.account.assets[] | select (."asset-id"==552946222) | .amount')
 
-echo $line $resultpool >> /home/k/gov/results/Gov1-B-POOL.txt
+INLIQUIDITY=$(echo $resultpool/$ASAPOOLRATIO*$ASAUNIT/1000000|bc)
+
+echo $line $INLIQUIDITY >> /home/k/gov/results/Gov1-B-tally.txt
 
 sed -i '1d' "/home/k/gov/Gov1-B.bak"
 
@@ -39,11 +53,13 @@ cp /home/k/gov/Gov2-A.txt /home/k/gov/Gov2-A.bak
 while IFS= read -r line; do
 resultafd=$(curl -X GET "https://mainnet-algorand.api.purestake.io/idx2/v2/accounts/${line}/" -H "x-api-key:API-KEY-HERE" -H 'accept: application/json' | jq -r '.account.assets[] | select (."asset-id"==393495312) | .amount')
 
-echo $line $resultafd >> /home/k/gov/results/Gov2-A-AFD.txt
+echo $line $resultafd >> /home/k/gov/results/Gov2-A-tally.txt
 
 resultpool=$(curl -X GET "https://mainnet-algorand.api.purestake.io/idx2/v2/accounts/${line}/" -H "x-api-key:API-KEY-HERE" -H 'accept: application/json' | jq -r '.account.assets[] | select (."asset-id"==552946222) | .amount')
 
-echo $line $resultpool >> /home/k/gov/results/Gov2-A-POOL.txt
+INLIQUIDITY=$(echo $resultpool/$ASAPOOLRATIO*$ASAUNIT/1000000|bc)
+
+echo $line $INLIQUIDITY >> /home/k/gov/results/Gov2-A-tally.txt
 
 sed -i '1d' "/home/k/gov/Gov2-A.bak"
 
@@ -55,11 +71,13 @@ cp /home/k/gov/Gov2-B.txt /home/k/gov/Gov2-B.bak
 while IFS= read -r line; do
 resultafd=$(curl -X GET "https://mainnet-algorand.api.purestake.io/idx2/v2/accounts/${line}/" -H "x-api-key:API-KEY-HERE" -H 'accept: application/json' | jq -r '.account.assets[] | select (."asset-id"==393495312) | .amount')
 
-echo $line $resultafd >> /home/k/gov/results/Gov2-B-AFD.txt
+echo $line $resultafd >> /home/k/gov/results/Gov2-B-tally.txt
 
 resultpool=$(curl -X GET "https://mainnet-algorand.api.purestake.io/idx2/v2/accounts/${line}/" -H "x-api-key:API-KEY-HERE" -H 'accept: application/json' | jq -r '.account.assets[] | select (."asset-id"==552946222) | .amount')
 
-echo $line $resultpool >> /home/k/gov/results/Gov2-B-POOL.txt
+INLIQUIDITY=$(echo $resultpool/$ASAPOOLRATIO*$ASAUNIT/1000000|bc)
+
+echo $line $INLIQUIDITY >> /home/k/gov/results/Gov2-B-tally.txt
 
 sed -i '1d' "/home/k/gov/Gov2-B.bak"
 
@@ -71,11 +89,13 @@ cp /home/k/gov/Gov2-C.txt /home/k/gov/Gov2-C.bak
 while IFS= read -r line; do
 resultafd=$(curl -X GET "https://mainnet-algorand.api.purestake.io/idx2/v2/accounts/${line}/" -H "x-api-key:API-KEY-HERE" -H 'accept: application/json' | jq -r '.account.assets[] | select (."asset-id"==393495312) | .amount')
 
-echo $line $resultafd >> /home/k/gov/results/Gov2-C-AFD.txt
+echo $line $resultafd >> /home/k/gov/results/Gov2-C-tally.txt
 
 resultpool=$(curl -X GET "https://mainnet-algorand.api.purestake.io/idx2/v2/accounts/${line}/" -H "x-api-key:API-KEY-HERE" -H 'accept: application/json' | jq -r '.account.assets[] | select (."asset-id"==552946222) | .amount')
 
-echo $line $resultpool >> /home/k/gov/results/Gov2-C-POOL.txt
+INLIQUIDITY=$(echo $resultpool/$ASAPOOLRATIO*$ASAUNIT/1000000|bc)
+
+echo $line $INLIQUIDITY >> /home/k/gov/results/Gov2-C-tally.txt
 
 sed -i '1d' "/home/k/gov/Gov2-C.bak"
 
@@ -87,11 +107,13 @@ cp /home/k/gov/Gov3-A.txt /home/k/gov/Gov3-A.bak
 while IFS= read -r line; do
 resultafd=$(curl -X GET "https://mainnet-algorand.api.purestake.io/idx2/v2/accounts/${line}/" -H "x-api-key:API-KEY-HERE" -H 'accept: application/json' | jq -r '.account.assets[] | select (."asset-id"==393495312) | .amount')
 
-echo $line $resultafd >> /home/k/gov/results/Gov3-A-AFD.txt
+echo $line $resultafd >> /home/k/gov/results/Gov3-A-tally.txt
 
 resultpool=$(curl -X GET "https://mainnet-algorand.api.purestake.io/idx2/v2/accounts/${line}/" -H "x-api-key:API-KEY-HERE" -H 'accept: application/json' | jq -r '.account.assets[] | select (."asset-id"==552946222) | .amount')
 
-echo $line $resultpool >> /home/k/gov/results/Gov3-A-POOL.txt
+INLIQUIDITY=$(echo $resultpool/$ASAPOOLRATIO*$ASAUNIT/1000000|bc)
+
+echo $line $INLIQUIDITY >> /home/k/gov/results/Gov3-A-tally.txt
 
 sed -i '1d' "/home/k/gov/Gov3-A.bak"
 
@@ -103,11 +125,13 @@ cp /home/k/gov/Gov3-B.txt /home/k/gov/Gov3-B.bak
 while IFS= read -r line; do
 resultafd=$(curl -X GET "https://mainnet-algorand.api.purestake.io/idx2/v2/accounts/${line}/" -H "x-api-key:API-KEY-HERE" -H 'accept: application/json' | jq -r '.account.assets[] | select (."asset-id"==393495312) | .amount')
 
-echo $line $resultafd >> /home/k/gov/results/Gov3-B-AFD.txt
+echo $line $resultafd >> /home/k/gov/results/Gov3-B-tally.txt
 
 resultpool=$(curl -X GET "https://mainnet-algorand.api.purestake.io/idx2/v2/accounts/${line}/" -H "x-api-key:API-KEY-HERE" -H 'accept: application/json' | jq -r '.account.assets[] | select (."asset-id"==552946222) | .amount')
 
-echo $line $resultpool >> /home/k/gov/results/Gov3-B-POOL.txt
+INLIQUIDITY=$(echo $resultpool/$ASAPOOLRATIO*$ASAUNIT/1000000|bc)
+
+echo $line $INLIQUIDITY >> /home/k/gov/results/Gov3-B-tally.txt
 
 sed -i '1d' "/home/k/gov/Gov3-B.bak"
 
@@ -119,11 +143,13 @@ cp /home/k/gov/Gov4-A.txt /home/k/gov/Gov4-A.bak
 while IFS= read -r line; do
 resultafd=$(curl -X GET "https://mainnet-algorand.api.purestake.io/idx2/v2/accounts/${line}/" -H "x-api-key:API-KEY-HERE" -H 'accept: application/json' | jq -r '.account.assets[] | select (."asset-id"==393495312) | .amount')
 
-echo $line $resultafd >> /home/k/gov/results/Gov4-A-AFD.txt
+echo $line $resultafd >> /home/k/gov/results/Gov4-A-tally.txt
 
 resultpool=$(curl -X GET "https://mainnet-algorand.api.purestake.io/idx2/v2/accounts/${line}/" -H "x-api-key:API-KEY-HERE" -H 'accept: application/json' | jq -r '.account.assets[] | select (."asset-id"==552946222) | .amount')
 
-echo $line $resultpool >> /home/k/gov/results/Gov4-A-POOL.txt
+INLIQUIDITY=$(echo $resultpool/$ASAPOOLRATIO*$ASAUNIT/1000000|bc)
+
+echo $line $INLIQUIDITY >> /home/k/gov/results/Gov4-A-tally.txt
 
 sed -i '1d' "/home/k/gov/Gov4-A.bak"
 
@@ -135,11 +161,13 @@ cp /home/k/gov/Gov4-B.txt /home/k/gov/Gov4-B.bak
 while IFS= read -r line; do
 resultafd=$(curl -X GET "https://mainnet-algorand.api.purestake.io/idx2/v2/accounts/${line}/" -H "x-api-key:API-KEY-HERE" -H 'accept: application/json' | jq -r '.account.assets[] | select (."asset-id"==393495312) | .amount')
 
-echo $line $resultafd >> /home/k/gov/results/Gov4-B-AFD.txt
+echo $line $resultafd >> /home/k/gov/results/Gov4-B-tally.txt
 
 resultpool=$(curl -X GET "https://mainnet-algorand.api.purestake.io/idx2/v2/accounts/${line}/" -H "x-api-key:API-KEY-HERE" -H 'accept: application/json' | jq -r '.account.assets[] | select (."asset-id"==552946222) | .amount')
 
-echo $line $resultpool >> /home/k/gov/results/Gov4-B-POOL.txt
+INLIQUIDITY=$(echo $resultpool/$ASAPOOLRATIO*$ASAUNIT/1000000|bc)
+
+echo $line $INLIQUIDITY >> /home/k/gov/results/Gov4-B-tally.txt
 
 sed -i '1d' "/home/k/gov/Gov4-B.bak"
 
@@ -151,11 +179,13 @@ cp /home/k/gov/Gov4-C.txt /home/k/gov/Gov4-C.bak
 while IFS= read -r line; do
 resultafd=$(curl -X GET "https://mainnet-algorand.api.purestake.io/idx2/v2/accounts/${line}/" -H "x-api-key:API-KEY-HERE" -H 'accept: application/json' | jq -r '.account.assets[] | select (."asset-id"==393495312) | .amount')
 
-echo $line $resultafd >> /home/k/gov/results/Gov4-C-AFD.txt
+echo $line $resultafd >> /home/k/gov/results/Gov4-C-tally.txt
 
 resultpool=$(curl -X GET "https://mainnet-algorand.api.purestake.io/idx2/v2/accounts/${line}/" -H "x-api-key:API-KEY-HERE" -H 'accept: application/json' | jq -r '.account.assets[] | select (."asset-id"==552946222) | .amount')
 
-echo $line $resultpool >> /home/k/gov/results/Gov4-C-POOL.txt
+INLIQUIDITY=$(echo $resultpool/$ASAPOOLRATIO*$ASAUNIT/1000000|bc)
+
+echo $line $INLIQUIDITY >> /home/k/gov/results/Gov4-C-tally.txt
 
 sed -i '1d' "/home/k/gov/Gov4-C.bak"
 
